6b7470acf786838bd54c520231f0fc463e3048cd
Abu Raihan <raihan2bd.official@gmail.com>
2023-01-22 17:44:39 +0600

    Join the dot in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
+import CartProvider from './store/CartProvider';
 import Header from './component/Layout/Header';
 import Meals from './component/Meals/Meals';
+import Cart from './component/Cart/Cart';
+import { useState } from 'react';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
