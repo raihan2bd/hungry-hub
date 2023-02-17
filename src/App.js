1148356803b0505e1d2c70de5341c827de5ccec8
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-17 23:04:59 +0600

    Initialize necessary routes to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import CartProvider from './store/CartProvider';
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrderedPage from './pages/OrderedPage';
+
 import { useState } from 'react';
 
 function App() {
@@ -21,7 +26,12 @@ function App() {
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
-        <Meals />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/auth' element={<AuthPage />} />
+          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path='/ordered' element={<OrderedPage />} />
+        </Routes>
       </main>
     </CartProvider>
   );
