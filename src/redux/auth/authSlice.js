71b24eee91b468e1c035be65efc64d30f85069b2
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-19 20:14:00 +0600

    Initialize the auth reducer to the project

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,89 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async ({ email, password, isLoggedIn }, { rejectWithValue }) => {
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_API_KEY}`;
+    if (isLoggedIn) {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_API_KEY}`;
+    }
+    try {
+      const response = await axios.post(
+        url,
+        {
+          email,
+          password,
+          returnSecureToken: true,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(response);
+    } catch (err) {
+      console.log(err)
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
+// initial state of auth slice
+const initialState = {
+  loading: false,
+  error: null,
+  token: null,
+  isAuth: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth-slice',
+  initialState,
+  reducers: {
+    logout(state) {
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        token: null,
+        isAuth: false,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        token: null,
+        isAuth: false,
+      };
+    });
+
+    builder.addCase(login.fulfilled, (state, { payload }) => {
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        token: payload.token,
+        isAuth: payload.isAuth,
+      };
+    });
+
+    builder.addCase(login.rejected, (state, { payload }) => {
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+        token: null,
+        isAuth: false,
+      };
+    });
+  },
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './auth/authSlice';
 import cartSlice from './cart/cartSlice';
 
 const store = configureStore({
   reducer: {
-    cart: cartSlice
+    auth: authSlice,
+    cart: cartSlice,
   }
 });
 
