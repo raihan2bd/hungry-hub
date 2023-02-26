8cfe8fbb87eb79c9421ee08730ddd385bb6b76e1
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-26 18:16:20 +0600

    Add About page to the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import CheckoutPage from './pages/CheckoutPage';
 import OrderedPage from './pages/OrderedPage';
+import About from './pages/About';
 import Layout from './components/Layout/Layout';
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/ordered" element={<OrderedPage />} />
+        <Route path="/order" element={<OrderedPage />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,5 @@
+const About = () => {
+  return <h2>Hello From About page!</h2>
+}
+
+export default About;
