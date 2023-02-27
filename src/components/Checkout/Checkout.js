f4852b6df2f1b64c77c64181db88c6d1dbd53fbe
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-27 22:29:51 +0600

    Add basic markup inside the checkout component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,5 +1,42 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import classes from './Checkout.module.css';
 const Checkout = () => {
-  return <h3>Welcome to checkout component</h3>
+  const {items, totalAmount} = useSelector((state) => state.cart)
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(items<=0) {
+      navigate('/', {replace: true})
+    }
+  }, [items, navigate])
+  return (
+    <div className={classes.container}>
+      <div className={classes.cart}>
+        <h2> Cart summery</h2>
+      <ul className={classes.cart}>
+        {items.map(item => (
+          <li key={item.id}
+          className={classes.cart_item}>
+            <h4>{item.name}</h4>
+            <span className={classes.amount}>x {item.amount}</span>
+            <p>{item.price}</p>
+          </li>
+        ))}
+      </ul>
+      <h3>Total Amount: {totalAmount}</h3>
+      </div>
+      <form>
+        <input type="text" placeholder="Enter your full name here.." />
+        <input type="text" placeholder="Enter your phone number here.." />
+        <input type="text" placeholder="Enter your country name here" />
+        <input type="text" placeholder="Enter your post code here" />
+        <input type="text" placeholder="Enter your Street/Address here" />
+        <button type="submit">Order</button>
+      </form>
+    </div>
+  )
 }
 
 export default Checkout;
