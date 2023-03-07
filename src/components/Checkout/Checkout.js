2ecafd6f27b9048c46d3888041bcee9ee31f2fbc
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-07 23:47:22 +0600

    Add Post cart items functionality to the server

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 import classes from './Checkout.module.css';
+import axios from 'axios';
+import { cartActions } from '../../redux/cart/cartSlice';
 const Checkout = () => {
   const [fullName, setFullname] = useState('');
+  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [country, setCountry] = useState('');
   const [postCode, setPostCode] = useState('');
@@ -12,11 +15,16 @@ const Checkout = () => {
   const [hasError, setHasError] = useState(true);
 
   const { items, totalAmount } = useSelector((state) => state.cart);
-  const authToken = useSelector((state) => state.auth.authToken);
+  const token = useSelector((state) => state.auth.token);
+  const userId = useSelector((state) => state.auth.userId);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const fullnameChangeHandler = (e) => {
     setFullname(e.target.value);
+  };
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value);
   };
   const phoneChangeHandler = (e) => {
     setPhone(e.target.value);
@@ -34,30 +42,45 @@ const Checkout = () => {
   const formSubmitHandler = (e) => {
     e.preventDefault();
     if (hasError) {
-      return
+      return;
     }
-    const cartData = {
+
+    const orderData = {
       Items: items,
       totalAmount: totalAmount,
-      fullName: fullName.trim(),
-      phone: phone.trim(),
-      country: country.trim(),
-      postCode: postCode.trim(),
-      address: address.trim(),
+      customarData: {
+        fullName: fullName.trim(),
+        email: email.trim(),
+        phone: phone.trim(),
+        country: country.trim(),
+        postCode: postCode.trim(),
+        address: address.trim(),
+      },
+      userId: userId,
     };
-    console.log(cartData);
-    
+    const baseUrl = `https://food-order-ed00a-default-rtdb.asia-southeast1.firebasedatabase.app`;
+    axios
+      .post(`${baseUrl}/orders.json?auth=${token}`, orderData)
+      .then((response) => {
+        // clear cart
+        dispatch(cartActions.clearCart());
+        navigate('/order', { replace: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  // useEffect(() => {
-  //   if(!authToken) {
-  //     navigate('/auth', {replace: true})
-  //   }
-  // }, [authToken, navigate])
+  useEffect(() => {
+    if (!token) {
+      navigate('/auth', { replace: true });
+    }
+  }, [token, navigate]);
 
   useEffect(() => {
     if (
       fullName.trim().length > 2 &&
+      email.trim().length > 7 &&
       phone.trim().length > 2 &&
       country.trim().length > 2 &&
       postCode.trim().length > 2 &&
@@ -67,85 +90,122 @@ const Checkout = () => {
     } else {
       setHasError(true);
     }
+  }, [fullName, email, phone, country, postCode, address]);
 
-    if (items.length <= 0) {
-      navigate('/', { replace: true });
-    }
-  }, [items, navigate, fullName, phone, country, postCode, address]);
+  let cartContent = (
+    <div className={classes.cart_summary} style={{ textAlign: 'center' }}>
+      <p>No Cart item found ! Please add a item to your cart!!</p>
+      <Link to="/">Back To Home</Link>
+    </div>
+  );
 
-  return (
-    <div className={classes.container}>
-      <div className={classes.cart_summary}>
-        <h2> Cart summary</h2>
-        <ul className={classes.cart_items}>
-          {items.map((item) => (
-            <li key={item.id} className={classes.cart_item}>
-              <h4>{item.name}</h4>
-              <span className={classes.amount}>x {item.amount}</span>
-              <p className={classes.cart_price}>{item.price}</p>
-            </li>
-          ))}
-        </ul>
-        <p className={classes.cart_total_price}>
-          Total Price: <span>{totalAmount.toFixed(2)}</span>
-        </p>
-      </div>
-      <div className={classes.checkout_form_section}>
+  if (items.length > 0) {
+    cartContent = (
+      <>
+        <div className={classes.cart_summary}>
+          <h2> Cart summary</h2>
+          <ul className={classes.cart_items}>
+            {items.map((item) => (
+              <li key={item.id} className={classes.cart_item}>
+                <h4>{item.name}</h4>
+                <span className={classes.amount}>x {item.amount}</span>
+                <p className={classes.cart_price}>{item.price}</p>
+              </li>
+            ))}
+          </ul>
+          <p className={classes.cart_total_price}>
+            Total Price: <span>{totalAmount.toFixed(2)}</span>
+          </p>
+        </div>
         <form className={classes.checkout_form} onSubmit={formSubmitHandler}>
           <h2>Checkout Form</h2>
           {hasError && (
             <p className={classes.has_error}>
-              All the fields is required and min it should be at least 3 char
+              All the fields is required and should be at least 3 charcter
               long !!
             </p>
           )}
           <fieldset className={classes.form_group}>
-            <input
-              type="text"
-              name="fullname"
-              onChange={fullnameChangeHandler}
-              placeholder="Enter your full name here.."
-              value={fullName}
-              required
-            />
-            <input
-              type="text"
-              name="phone"
-              onChange={phoneChangeHandler}
-              placeholder="Enter your phone number here.."
-              value={phone}
-              required
+            <div className={classes.form_control}>
+              <label htmlFor="fullname">Full Name:</label>
+              <input
+                type="text"
+                name="fullname"
+                id="fullname"
+                onChange={fullnameChangeHandler}
+                placeholder="Enter full name"
+                value={fullName}
+                required
               />
-            <input
-              type="text"
-              name="country"
-              onChange={countryChangeHandler}
-              placeholder="Enter your country name here"
-              value={country}
-              required
+            </div>
+            <div className={classes.form_control}>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                onChange={emailChangeHandler}
+                placeholder="Enter email"
+                value={email}
+                required
               />
-            <input
-              type="text"
-              name="post_code"
-              onChange={postCodeChangeHandler}
-              placeholder="Enter your post code here"
-              value={postCode}
-              required
-            />
-            <input
-              type="text"
-              name="address"
-              onChange={addressChangeHandler}
-              placeholder="Enter your Street/Address here"
-              value={address}
-              required
-            />
+            </div>
+            <div className={classes.form_control}>
+              <label htmlFor="phone">Phone:</label>
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                onChange={phoneChangeHandler}
+                placeholder="Enter phone number"
+                value={phone}
+                required
+              />
+            </div>
+            <div className={classes.form_control}>
+              <label htmlFor="country">Country:</label>
+              <input
+                type="text"
+                id="country"
+                name="country"
+                onChange={countryChangeHandler}
+                placeholder="Enter country name"
+                value={country}
+                required
+              />
+            </div>
+            <div className={classes.form_control}>
+              <label htmlFor="post_code">Post Code:</label>
+              <input
+                type="text"
+                id="post_code"
+                name="post_code"
+                onChange={postCodeChangeHandler}
+                placeholder="Enter post code"
+                value={postCode}
+                required
+              />
+            </div>
+            <div className={classes.form_control}>
+              <label htmlFor="address">Address:</label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                onChange={addressChangeHandler}
+                placeholder="Enter Street/Address"
+                value={address}
+                required
+              />
+            </div>
             <button type="submit">Order</button>
           </fieldset>
         </form>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
+
+  return <div className={classes.container}>{cartContent}</div>;
 };
 
 export default Checkout;
