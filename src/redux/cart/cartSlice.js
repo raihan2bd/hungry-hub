7dffa1c918f61e942b8b134d4bb09b8ea5af9e16
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-07 23:48:18 +0600

    Add clear cart action to the cartSlice

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -2,45 +2,44 @@ import { createSlice } from '@reduxjs/toolkit';
 
 // const addItem = createAction('cart/addItem')
 
-
 const initialState = {
   items: [],
   totalAmount: 0,
-  cartIsShown: false
-}
+  cartIsShown: false,
+};
 
 const cartSlice = createSlice({
-  name: "cart-slice",
+  name: 'cart-slice',
   initialState,
   reducers: {
-    addItem (state, { payload }) {
-        const updatedTotalAmount =
-          state.totalAmount + payload.price * payload.amount;
-        const existingCartItemIndex = state.items.findIndex(
-          (item) => payload.id === item.id
-        );
-    
-        const existingCartItem = state.items[existingCartItemIndex];
-        let updatedItems;
-    
-        if (existingCartItem) {
-          const updatedItem = {
-            ...existingCartItem,
-            amount: existingCartItem.amount + payload.amount,
-          };
-          updatedItems = [...state.items];
-          updatedItems[existingCartItemIndex] = updatedItem;
-        } else {
-          updatedItems = state.items.concat(payload);
-        }
-        return {
-          items: updatedItems,
-          totalAmount: updatedTotalAmount,
+    addItem(state, { payload }) {
+      const updatedTotalAmount =
+        state.totalAmount + payload.price * payload.amount;
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => payload.id === item.id
+      );
+
+      const existingCartItem = state.items[existingCartItemIndex];
+      let updatedItems;
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + payload.amount,
         };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = state.items.concat(payload);
+      }
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
     },
 
     // removeItem is remove item from state
-    removeItem (state, { payload }) {
+    removeItem(state, { payload }) {
       const existingCartItemIndex = state.items.findIndex(
         (item) => item.id === payload
       );
@@ -57,30 +56,37 @@ const cartSlice = createSlice({
         };
         updatedItems[existingCartItemIndex] = updatedItem;
       }
-  
+
       return {
         items: updatedItems,
         totalAmount: updatedTotalAmount,
       };
     },
 
-    showCart (state) {
+    clearCart() {
+      return {
+        items: [],
+        totalAmount: 0,
+        cartIsShown: false,
+      };
+    },
+
+    showCart(state) {
       return {
         ...state,
         cartIsShown: true,
-      }
+      };
     },
 
-    hideCart (state) {
+    hideCart(state) {
       return {
         ...state,
         cartIsShown: false,
-      }
-    }
-  }
+      };
+    },
+  },
 });
 
 export const cartActions = cartSlice.actions;
 
 export default cartSlice.reducer;
-
