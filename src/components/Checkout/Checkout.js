43dc798bb58244f39ad3a1ef3e1fc5bb54626964
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-07 20:58:53 +0600

    Add nacassary form inputs to the checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,16 +1,78 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import classes from './Checkout.module.css';
 const Checkout = () => {
+  const [fullName, setFullname] = useState('');
+  const [phone, setPhone] = useState('');
+  const [country, setCountry] = useState('');
+  const [postCode, setPostCode] = useState('');
+  const [address, setAddress] = useState('');
+  const [hasError, setHasError] = useState(true);
+
   const { items, totalAmount } = useSelector((state) => state.cart);
+  const authToken = useSelector((state) => state.auth.authToken);
   const navigate = useNavigate();
+
+  const fullnameChangeHandler = (e) => {
+    setFullname(e.target.value);
+  };
+  const phoneChangeHandler = (e) => {
+    setPhone(e.target.value);
+  };
+  const countryChangeHandler = (e) => {
+    setCountry(e.target.value);
+  };
+  const postCodeChangeHandler = (e) => {
+    setPostCode(e.target.value);
+  };
+  const addressChangeHandler = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (hasError) {
+      return
+    }
+    const cartData = {
+      Items: items,
+      totalAmount: totalAmount,
+      fullName: fullName.trim(),
+      phone: phone.trim(),
+      country: country.trim(),
+      postCode: postCode.trim(),
+      address: address.trim(),
+    };
+    console.log(cartData);
+    
+  };
+
+  // useEffect(() => {
+  //   if(!authToken) {
+  //     navigate('/auth', {replace: true})
+  //   }
+  // }, [authToken, navigate])
+
   useEffect(() => {
-    if (items <= 0) {
+    if (
+      fullName.trim().length > 2 &&
+      phone.trim().length > 2 &&
+      country.trim().length > 2 &&
+      postCode.trim().length > 2 &&
+      address.trim().length > 2
+    ) {
+      setHasError(false);
+    } else {
+      setHasError(true);
+    }
+
+    if (items.length <= 0) {
       navigate('/', { replace: true });
     }
-  }, [items, navigate]);
+  }, [items, navigate, fullName, phone, country, postCode, address]);
+
   return (
     <div className={classes.container}>
       <div className={classes.cart_summary}>
@@ -24,20 +86,63 @@ const Checkout = () => {
             </li>
           ))}
         </ul>
-        <p className={classes.cart_total_price}>Total Price: <span>{totalAmount.toFixed(2)}</span></p>
+        <p className={classes.cart_total_price}>
+          Total Price: <span>{totalAmount.toFixed(2)}</span>
+        </p>
       </div>
       <div className={classes.checkout_form_section}>
-      <form className={classes.checkout_form}>
-        <h2>Checkout Form</h2>
-        <fieldset className={classes.form_group}>
-        <input type="text" placeholder="Enter your full name here.." />
-        <input type="text" placeholder="Enter your phone number here.." />
-        <input type="text" placeholder="Enter your country name here" />
-        <input type="text" placeholder="Enter your post code here" />
-        <input type="text" placeholder="Enter your Street/Address here" />
-        <button type="submit">Order</button>
-        </fieldset>
-      </form>
+        <form className={classes.checkout_form} onSubmit={formSubmitHandler}>
+          <h2>Checkout Form</h2>
+          {hasError && (
+            <p className={classes.has_error}>
+              All the fields is required and min it should be at least 3 char
+              long !!
+            </p>
+          )}
+          <fieldset className={classes.form_group}>
+            <input
+              type="text"
+              name="fullname"
+              onChange={fullnameChangeHandler}
+              placeholder="Enter your full name here.."
+              value={fullName}
+              required
+            />
+            <input
+              type="text"
+              name="phone"
+              onChange={phoneChangeHandler}
+              placeholder="Enter your phone number here.."
+              value={phone}
+              required
+              />
+            <input
+              type="text"
+              name="country"
+              onChange={countryChangeHandler}
+              placeholder="Enter your country name here"
+              value={country}
+              required
+              />
+            <input
+              type="text"
+              name="post_code"
+              onChange={postCodeChangeHandler}
+              placeholder="Enter your post code here"
+              value={postCode}
+              required
+            />
+            <input
+              type="text"
+              name="address"
+              onChange={addressChangeHandler}
+              placeholder="Enter your Street/Address here"
+              value={address}
+              required
+            />
+            <button type="submit">Order</button>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
