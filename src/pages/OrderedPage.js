75e6526aa1c63d2c969c07fe0c577de361e403a6
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-08 13:20:04 +0600

    Add necessary styles to the Order page

--- src/pages/OrderedPage.js
+++ src/pages/OrderedPage.js
@@ -1,29 +1,45 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchOrders } from '../redux/order/orderSlice';
 
 import Order from '../components/Order/Order';
+import classes from './OrderedPage.module.css';
 
 const OrderedPage = () => {
   const { token, userId } = useSelector((state) => state.auth);
-  const orders = useSelector((state) => state.orders.orders)
+  const orders = useSelector((state) => state.orders.orders);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(!token) {
-      navigate('/auth')
-      return
+    if (!token) {
+      navigate('/auth');
+      return;
     }
-   dispatch(fetchOrders({token, userId}))
+    dispatch(fetchOrders({ token, userId }));
   }, [token, userId, dispatch, navigate]);
+
+  let ordersContent = (
+    <p>
+      No ordered found!! Please order new food item now!! <Link to="/">Back to Home</Link>
+    </p>
+  );
+  if (orders.length > 0) {
+    ordersContent = (
+      <div className={classes.container}>
+        <h2 className={classes.title}>Your Orders ({orders.length})</h2>
+        <ul className={classes.orders}>
+          {orders.map((order) => (
+            <Order key={order.id} order={order} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
   return (
     <section>
-      <h2> Hello from ordered page!!</h2>
-      {orders.map(order => (
-        <Order key={order.id} />
-      ))}
+     {ordersContent}
     </section>
   );
 };
