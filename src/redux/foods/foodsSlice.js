f648f297e3a6c81118ea404b3a2572942b44364b
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-09 00:53:22 +0600

    Add fetch foods functionality from the server to the project

--- src/redux/foods/foodsSlice.js
+++ src/redux/foods/foodsSlice.js
@@ -1,5 +1,23 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+const baseUrl = 'https://food-order-ed00a-default-rtdb.asia-southeast1.firebasedatabase.app'
+
+export const fetchFoods = createAsyncThunk('foods/fetch', async (data, {rejectWithValue}) => {
+  try {
+    const response = await axios.get(`${baseUrl}/${data}.json`)
+    const fetchedFoods = [];
+    for(let key in response.data) {
+      fetchedFoods.push({
+        ...response.data[key],
+        id: key
+      })
+    }
+    return fetchedFoods;
+  } catch (err) {
+    rejectWithValue(err.message)
+  }
+})
 
 const initialState = {
   foods: [],
@@ -12,6 +30,32 @@ const foodsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    builder.addCase(fetchFoods.pending, (state) => {
+      return {
+        ...state,
+        foods: [],
+        isLoading: true,
+        error: null
+      }
+    });
+
+    builder.addCase(fetchFoods.fulfilled, (state, {payload}) => {
+      return {
+        ...state,
+        foods: [...payload],
+        isLoading: false,
+        error: null
+      }
+    });
+
+    builder.addCase(fetchFoods.rejected, (state, {payload}) => {
+      return {
+        ...state,
+        foods: [],
+        isLoading: false,
+        error: payload|| "Internal server error!"
+      }
+    });
   }
 });
 
