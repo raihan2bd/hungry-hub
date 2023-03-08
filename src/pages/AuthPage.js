e576c6ecad9caadb17091a920fcbbbbe10568bc9
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-08 22:53:53 +0600

    Modify authentication page design

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+
+
 import Auth from '../components/Auth/Auth';
+import bgImg from '../assets/food-wallpaper.jpg';
 import classes from './AuthPage.module.css';
 
 const AuthPage = () => (
-  <section className={classes.auth_section}>
+  <section style={{backgroundImage: `url(${bgImg})`}} className={classes.auth_section}>
     <Auth />
   </section>
 );
